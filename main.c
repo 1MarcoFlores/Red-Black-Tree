37bf8f03d4f3aa844b967eec6e7add017a1bbc7b
Ntl21hub <70297372+Ntl21hub@users.noreply.github.com>
2020-10-19 21:36:47 -0500

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,30 @@
 
 int main(int argc, char **argv)
 {
+	srandom(time(NULL));
+	int i=0,N=12;
+	
+	Tree *arblRB=NULL;
+	//printf ("%d\n",sizeof(RBTree));
+	
+	//Inicializacion
+	arblRB=(Tree *)malloc(sizeof(Tree));
+		if(arblRB!=NULL){
+			arblRB->raiz=NULL;
+			//arblRB->nil=(RBTree *)malloc(sizeof(RBTree));
+			//arblRB->nil->color='M';
+			arblRB->nodos=0;
+		}
+	
+	for(i = 0; i < N; i++){
+		put(arblRB,random()%20,i);
+		InorderTree(arblRB,arblRB->raiz);
+		printf(" i=%d\n",i);
+		printf("nodo padre%d\n",arblRB->raiz->key);
+	}
+	InorderTree(arblRB,arblRB->raiz);
+	printf("nodo padre%d\n",arblRB->raiz->key);
+	printf("size%d\n", size(arblRB));
 	
 	return 0;
 }
